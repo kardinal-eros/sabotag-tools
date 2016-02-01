027e5b11b6177f8cade2730ef83f0713456ec8a0
Roland Kaiser <kardinal.eros@gmail.com>
2016-02-01 20:32:27 +0100

    added pretty method

--- pkg/R/floragrid.R
+++ pkg/R/floragrid.R
@@ -1,5 +1,5 @@
 floragrid <-
-function (extent) {
+function (extent, resolution = "CELL") {
 		if (missing(extent)) stop("plese supply exent")
 					
 		if (inherits(extent, "Extent") | inherits(extent, "Spatial")) {
@@ -7,31 +7,20 @@ function (extent) {
 		} else {
 			stop("extent must be an Extent of Spatial* object")			
 		}
-	
-		#	x resolution: 5 / 60 
-		#	y resolution: 3 / 60
-		interval <- function (x, y) {
-			sort(floor(x) + seq(-1, 1, by = y / 60))
-		}	
 
-		min <- function (x, ...) {
-			i <- interval(x, ...)
-			i [ findInterval(x, i) ]
-		}
-		max <- function (x, ...) {
-			i <- interval(x, ...)
-			i [ findInterval(x, i) + 1 ]
-		}
-	
-		e <- extent(c(min(xmin(e), 5), max(xmax(e), 5),
-		min(ymin(e), 3), max(ymax(e), 3)) )
-	
-		r <- raster(extent(e), res = c(5 / 60, 3 / 60), crs = "+init=epsg:4326")
+		RESOLUTION <- c("GRID", "CELL")
+		resolution <- match.arg(resolution, RESOLUTION, several.ok = FALSE)
+
+		if (resolution == "GRID") xx <- 10; yy <- 6
+		if (resolution == "CELL") xx <- 5; yy <- 3
+				
+		r <- raster(pretty(e, resolution = resolution),
+			res = c(xx / 60, yy / 60), crs = "+init=epsg:4326")
 		r <- as(r, "SpatialPolygons")
 
 		d <- data.frame(t(apply(coordinates(r), 1,
-		function (x) lnglat2gridcell(x[ 1 ], x[ 2 ]))),
-		stringsAsFactors = FALSE)
+			function (x) lnglat2gridcell(x[ 1 ], x[ 2 ]))),
+			stringsAsFactors = FALSE)
 	
 		r <- SpatialPolygonsDataFrame(r, d)
 
---
+++ pkg/R/pretty.R
@@ -0,0 +1,42 @@
+#	internal functions for pretty to get calculate of extent
+.interval <-
+function (x, ...) {
+	sort(floor(x) + seq(-1, 1, by = ... / 60))	
+}	
+
+.min <-
+function (x, ...) {
+	i <- .interval(x, ...)
+	i [ findInterval(x, i) ]
+}
+.max <- function (x, ...) {
+	i <- .interval(x, ...)
+	i [ findInterval(x, i) + 1 ]
+}
+
+if (!isGeneric("pretty")) {
+	setGeneric("pretty", function (x, ...) standardGeneric("pretty"))
+}
+
+setMethod("pretty",
+	signature(x = "Extent"),
+	function (x, resolution, ...) {
+		if (missing(resolution)) {
+			resolution <- "CELL"
+			message("set \"CELL\" (5' x 3') as default resolution ")
+			#	message("set \"GRID\" (10' x 6') as default resolution ")
+		}
+		
+		RESOLUTION <- c("GRID", "CELL")
+		resolution <- match.arg(resolution, RESOLUTION, several.ok = FALSE)
+		
+		if (resolution == "GRID") xx <- 10; yy <- 6
+		if (resolution == "CELL") xx <- 5; yy <- 3
+
+		r <- extent(c(
+		.min(xmin(x), xx), .max(xmax(x), xx),
+		.min(ymin(x), yy), .max(ymax(x), yy)) )
+		
+		return(r)		
+		}	
+)
