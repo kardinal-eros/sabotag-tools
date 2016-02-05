3d635f401a5d7244a82ce3e0c0e30fa75faccd3c
Roland Kaiser <kardinal.eros@gmail.com>
2016-02-05 14:16:23 +0100

    added simple function to read export from Zobodat online database

---
+++ pkg/R/zobodat.R
@@ -0,0 +1,28 @@
+zobodat <- function (x) {
+	stopifnot(file.exists(x))
+	#	decimal in coordiantes is dot but column sperator is semicolon as used throughout german speaking countries
+	x <- read.csv(x, fileEncoding = "ISO_8859-1", stringsAsFactors = FALSE,
+		dec = ".", sep = ";")
+	#	substitue non-ASCII characters
+	names(x) <- gsub("\u00E4", "ae", names(x))
+	names(x) <- gsub("\u00F6", "oe", names(x))
+	#	discards observation withj missing coordiantes
+	if ( any(is.na(x$Laenge)) | any(is.na(x$Breite)) ) {
+		i <- !is.na(x$Laenge) & !is.na(x$Breite)
+		x <- x[ i, ]
+	}
+	#	for safty
+	i <- x$Laenge != "" & x$Breite != ""
+	x <- x[ i, ]
+		
+	#	add sign to coordinates
+	i <- x$E.W == "W"
+	x$Laenge[ i ] <- x$Laenge[ i ] * -1
+	
+	i <- x$N.S == "S"
+	x$Breite[ i ] <- x$Breite[ i ] * -1
+	
+	coordinates(x) <- ~ Laenge + Breite
+	proj4string(x) <- CRS("+init=epsg:4326")
+	
+}
