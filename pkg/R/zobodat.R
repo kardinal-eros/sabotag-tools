387a854459e9e0ccc6c6034a6efc0ba97ca1b0e7
Roland Kaiser <kardinal.eros@gmail.com>
2016-02-07 23:03:44 +0100

    extended zobodat function

--- pkg/R/zobodat.R
+++ pkg/R/zobodat.R
@@ -1,36 +1,28 @@
-zobodat <- function (x) {
-	stopifnot(file.exists(x))
-	#	decimal in coordiantes is dot but column sperator is semicolon as used throughout german speaking countries
-	x <- read.csv(x, fileEncoding = "ISO_8859-1", stringsAsFactors = FALSE,
-		dec = ".", sep = ";")
-	#	substitue non-ASCII characters
-	names(x) <- gsub("\u00E4", "ae", names(x))
-	names(x) <- gsub("\u00F6", "oe", names(x))
-	#	discards observation withj missing coordiantes
-	if ( any(is.na(x$Laenge)) | any(is.na(x$Breite)) ) {
-		i <- !is.na(x$Laenge) & !is.na(x$Breite)
-		x <- x[ i, ]
-	}
-	#	for safty
-	i <- x$Laenge != "" & x$Breite != ""
-	x <- x[ i, ]
-	
-	#	if matrix is empty
-	if (nrow(x) == 0) {
-		warning("no observations with coordinates", call. = FALSE)
-		return(NULL)		
-	}	
-		
-	#	add sign to coordinates
-	i <- x$E.W == "W"
-	x$Laenge[ i ] <- x$Laenge[ i ] * -1
-	
-	i <- x$N.S == "S"
-	x$Breite[ i ] <- x$Breite[ i ] * -1
-	
-	coordinates(x) <- ~ Laenge + Breite
-	proj4string(x) <- CRS("+init=epsg:4326")
-	
-	return(x)
-	
+zobodat <-
+function (genus, species, file) {
+
+if (missing(file)) {
+	file <- tempfile()
+} else {
+	stopifnot(is.character(file))
+}
+#	decoded url
+#	as_b[0][i]=gattung&as_b[0][qt]=contains&as_b[0][v]=Poa&as_b[1][i]=art&as_b[1][qt]=contains&as_b[1][v]=nemoralis
+url <- "http://www.zobodat.at/belege_csv.php?as_b%5B0%5D%5Bi%5D=gattung&as_b%5B0%5D%5Bqt%5D=contains&as_b%5B0%5D%5Bv%5D=GENUS&as_b%5B1%5D%5Bi%5D=art&as_b%5B1%5D%5Bqt%5D=contains&as_b%5B1%5D%5Bv%5D=SPECIES"
+
+url <- gsub("GENUS", genus, url)
+url <- gsub("SPECIES", species, url)
+# ask server to set some cookies
+h <- new_handle()
+r <- curl_fetch_memory("http://www.zobodat.at/belege.php", handle = h)
+#handle_cookies(h)
+
+
+message("waiting for server, be patient")
+con <- curl_fetch_disk(url, file, handle = h)
+
+if (con$status_code == 200)
+r <- zobodat.csv(file)
+
+return(r)
 }
