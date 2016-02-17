6c77d28786e76b36d67c13c90e157adb24162084
Roland Kaiser <kardinal.eros@gmail.com>
2016-02-17 11:21:08 +0100

    cleaned indents

--- pkg/R/AAA.R
+++ pkg/R/AAA.R
@@ -7,7 +7,7 @@
 	if (multi) {
 		r$GeoLaenge <- as.numeric(r$GeoLaenge)
 		r$GeoBreite <- as.numeric(r$GeoBreite)
-		coordinates(r) <- ~GeoLaenge+GeoBreite  	
+		coordinates(r) <- ~GeoLaenge+GeoBreite
 	} else {
 		xy <- matrix(unlist(x$coordinate), ncol = 2, nrow = nrow(x$data), byrow = TRUE)[ , c(2,1)]
 		coordinates(r) <- xy
