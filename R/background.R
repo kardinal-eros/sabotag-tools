1e01cf9e2428eab19fc2631625dfad2c3049df9b
Roland Kaiser <kardinal.eros@gmail.com>
2017-12-18 19:46:51 +0100

    started coding background plotting function for atlas generation

---
+++ R/background.R
@@ -0,0 +1,13 @@
+#library(sabotag)
+#library(sabotagdata)
+
+#data(BEV)
+#extent <- extent(BEV[BEV$BL == "Salzburg", ])
+
+#background <- 
+#function (extent) {
+#	e <- pretty(extent, resolution = "GRID", add = 2)
+#	x <- floragrid(e, resolution = "GRID")
+#	plot(x)
+#	plot(sbg, add = T, col = 2)
+#}
