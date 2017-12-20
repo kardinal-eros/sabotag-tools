3bd12a87ec56f842daab98432f60a60ca43aebeb
Roland Kaiser <kardinal.eros@gmail.com>
2017-12-20 19:51:12 +0100

    cleaned code

--- R/lnglat2gridcell.R
+++ R/lnglat2gridcell.R
@@ -1,7 +1,6 @@
 lnglat2gridcell <-
 function (x, y) {
-	#	Niklfeld, H. (1978): Grundfeldschlüssel für die Kartierung der Flora Mitteleuropas, (Zentralstelle für Florenkartierung, Inst. Bot. Univ. Wien). Wien.
-	#	alogrithm from http://www.oeaw.ac.at/isr/raumalp/raumalp/rasterraum.html
+	#	FORTRAN alogrithm from http://www.oeaw.ac.at/isr/raumalp/raumalp/rasterraum.html
 
 	#	input:  longitude [XCOORD], latitude [YCOORD]
 	#	output: gridcell [RASTERF] as HHRRQ (Long Integer)
@@ -12,20 +11,22 @@ function (x, y) {
 	#	  IIf(((([ YCOORD ] - Int([ YCOORD ])) * 60) Mod 6)  <=3,3,1))
 	
 	stopifnot(is.numeric(x)); stopifnot(is.numeric(y))	
+	
+	#	grid cell where cell is zero (length 5)
+	GRID  <- ( (floor(10 * (56 - y) ) * 1000) ) + ( floor(6 * (x - 6) + 2) * 10 )
+	#	find cell
+	test1 <- ( (x - floor(x) ) * 60 ) %% 10
+	test2 <- ( (y - floor(y) ) * 60 ) %% 6
 
-	grid  <- ( (floor(10 * (56 - y)) * 1000)) + (floor(6 * (x - 6) + 2) * 10 )
-	test1 <- ( (x - floor(x)) * 60 ) %% 10
-	test2 <- ( (y - floor(y)) * 60 ) %% 6
-
-	cell <- 0	
-	if (test1 <= 5) cell <- 0 else cell <- 1
-		if (test2 == 0) cell <- cell + 1
-			if (test2 <= 3) cell <- cell + 3 else cell <- cell + 1
+	CELL <- 0
+	if (test1 <= 5) CELL <- 0 else CELL <- 1
+		if (test2 == 0) CELL <- CELL + 1
+			if (test2 <= 3) CELL <- CELL + 3 else CELL <- CELL + 1
 	
 	r <- c(
-		GRIDCELL = paste(sprintf("%04d", grid / 10), cell, sep = "-"),
-		GRID = sprintf("%04d", grid / 10),
-		CELL = cell
+		GRIDCELL = paste(sprintf("%04d", GRID / 10), CELL, sep = "-"),
+		GRID = sprintf("%04d", GRID / 10),
+		CELL = CELL
 		)		
 
 		return(r)
