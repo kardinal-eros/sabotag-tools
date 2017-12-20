cd1681cc188ba5eb81968eb90a7950d46a91c255
Roland Kaiser <kardinal.eros@gmail.com>
2017-12-20 19:51:23 +0100

    added tests

---
+++ tests/gridcell.R
@@ -0,0 +1,11 @@
+library(sabotag)
+
+#	test if gridcell2lnglat and lnglat2gridcell return the same values
+r1 <- gridcell2lnglat("8144-3")
+
+r2 <- lnglat2gridcell(r1[1], r1[2])
+
+g <- floragrid(extent(12,14,47,48))
+
+#	precision with 12 digits
+sum(round(c(coordinates(g[ g$GRIDCELL == "8144-3", ])) - r1, 12)) == 0
